0fdffc65e5813c9a506fc365bdcfdd25a58feca4
filipibc <filipibc@gmail.com>
2022-06-30 09:15:40 -0300

    Criando a solution Criando Console Application

---
+++ StringArrayToString/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace StringArrayToString;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("----- How to Convert String Array into String in C# -----");
+
+        Console.ReadLine();
+    }
+}
